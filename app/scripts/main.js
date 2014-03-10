71240dbdc544e7637bb37b7bd96cff3d616cd64e
Anselm Hannemann <info@anselm-hannemann.com>
2014-03-10 22:08:37 +0100

    Add em value in select and conversion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,18 @@
   var sizerSoze = new SizerSoze();
 
   var urlInput = doc.querySelector( "input[name=url]" );
+  var requestBtn = doc.querySelector( "#requestresults" );
+
+  requestBtn.addEventListener( "click", function(e) {
+    sizerSoze.sizerTime({
+      loading: "images/ajax-loader.gif"
+    });
+  });
 
   urlInput.addEventListener( "keyup" , function( e ){
     if( e.which === 13 ) {
       sizerSoze.sizerTime({
         loading: "images/ajax-loader.gif"
-        // distractionYTVideo: "Of2HU3LGdbo"
       });
     }
   });
